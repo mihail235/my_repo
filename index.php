51db4c6b30e90434cfdf920f53e344ccb7eb5a59
mihail235 <mihailrakceev235@gmail.com>
2024-10-28 14:14:40 +0300

    my_first_commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$number = intval(readline("Введите цифру от 1 до 5: "));
+    case 1:
+        echo "Вы ввели единицу.\n";
+        break;
+    case 2:
+        echo "Вы ввели двойку.\n";
+        break;
+    case 3:
+        echo "Вы ввели тройку.\n";
+        break;
+    case 4:
+        echo "Вы ввели четверку.\n";
+        break;
+    case 5:
+        echo "Вы ввели пятерку.\n";
+        break;
+    default:
+        echo "Неправильный ввод. Пожалуйста, введите цифру от 1 до 5.\n";
+?>
