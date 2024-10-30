5ab7af4f8e785c8f2ee51eb55dcb712597e3e7c3
alimaga888 <159657665+alimaga888@users.noreply.github.com>
2024-10-30 19:34:16 +0300

    Изменение_ураааа

--- index.php
+++ index.php
@@ -1,22 +1,28 @@
 <?php
-$number = intval(readline("Введите цифру от 1 до 5: "));
-switch ($number) {
-    case 1:
-        echo "Вы ввели единицу!\n";
-        break;
-    case 2:
-        echo "Вы ввели двойку.\n";
-        break;
-    case 3:
-        echo "Вы ввели тройку.\n";
-        break;
-    case 4:
-        echo "Вы ввели четверку.\n";
-        break;
-    case 5:
-        echo "Вы ввели пятерку.\n";
-        break;
-    default:
-        echo "Неправильный ввод. Пожалуйста, введите цифру от 1 до 5.\n";
-}
+do {
+    $number = intval(readline("Введите цифру от 1 до 5 (или 0 для выхода): "));
+    
+    switch ($number) {
+        case 1:
+            echo "Вы ввели единицу!\n";
+            break;
+        case 2:
+            echo "Вы ввели двойку.\n";
+            break;
+        case 3:
+            echo "Вы ввели тройку.\n";
+            break;
+        case 4:
+            echo "Вы ввели четверку.\n";
+            break;
+        case 5:
+            echo "Вы ввели пятерку.\n";
+            break;
+        case 0:
+            echo "Выход из программы.\n";
+            break;
+        default:
+            echo "Неправильный ввод. Пожалуйста, введите цифру от 1 до 5.\n";
+    }
+} while ($number !== 0);
 ?>
